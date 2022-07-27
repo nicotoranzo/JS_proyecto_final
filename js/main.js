c839724c86bfd4ac2b23c2dc4f4223dbe0baa34c
Nicolas Toranzo <toranzo.nicolas@gmail.com>
2022-07-27 16:07:12 -0300

    preentrega

--- js/main.js
+++ js/main.js
@@ -12,25 +12,23 @@ function Prestamo(capital, cantidadCuotas, interes, cuotaMes, devolucionTotal) {
 
 if(cantidadPrestamos<=2){
 
-    let prestamo = new Prestamo(0,0,0,0,0)
-    let Prestamos = []
+    let prestamos = []
 
     for(let i = 0; i < cantidadPrestamos; i++){
 
+        let prestamo = new Prestamo(0,0,0,0,0)
         prestamo.capital = parseInt(prompt("ingresa el monto a pedir prestado (minimo $10.000)"))
         prestamo.cantidadCuotas = parseInt(prompt("ingresa la cantidad de cuotas que queres pagar (maximo 60)"))
         prestamo.interes = parseFloat(prompt("ingresa el interes anual que queres pagar")) / 100
 
         function tasa (interes){
-        return (1 + prestamo.interes) ** (1/12) - 1
+        return (1 + interes) ** (1/12) - 1
         }; 
-
         function cuotaMensual (capital, interes, cuotas){
-        prestamo.cuotaMes = (tasa(prestamo.interes) * prestamo.capital) / (1 - (1 + tasa(prestamo.interes)) ** - prestamo.cantidadCuotas)
+        prestamo.cuotaMes = (tasa(interes) * capital) / (1 - (1 + tasa(interes)) ** - cuotas)
         }; 
-
         function devolucionTotal(cuotaMensual, cuotas){
-        prestamo.devolucionTotal = prestamo.cuotaMes * prestamo.cantidadCuotas    
+        prestamo.devolucionTotal = cuotaMensual * cuotas    
         };
 
         if(prestamo.capital >= 10000){
@@ -38,31 +36,35 @@ if(cantidadPrestamos<=2){
                 tasa(prestamo.interes)
                 cuotaMensual(prestamo.capital, tasa(prestamo.interes), prestamo.cantidadCuotas)
                 devolucionTotal(prestamo.cuotaMes, prestamo.cantidadCuotas) 
-                Prestamos.push(prestamo)  
-                for (const prestamo of Prestamos){
-                    /*alert("los datos de tu prestamo son: \n Capital: $ " + prestamo.capital + 
-                        "\n Interes: " + prestamo.interes * 100 + "%" 
-                        + "\n Cantidad de Cuotas: " + prestamo.cantidadCuotas
-                        + "\n Cuota Mensual: $ " + prestamo.cuotaMes.toFixed(2) + 
-                        "\n Devolución total: $" + prestamo.devolucionTotal.toFixed(2))*/
-                        let card = document.createElement("div")
-                        card.innerHTML=`<h3>Solicitaste: $${prestamo.capital}</h3>
-                                        <p>vas a pagar: $${prestamo.cuotaMes.toFixed(2)}</p>
-                                        <p>en: ${prestamo.cantidadCuotas} cuotas</p>`
-                        document.body.append(card) 
-                }
+                prestamos.push(prestamo)  
+
            }
            else{
-             alert("Excediste la cantidad de cuotas")
+                let card = document.createElement("div")
+                card.innerHTML=`<h3>Excediste la cantidad de cuotas habilitadas</h3>                                        `
+                document.body.append(card) 
            } 
         }
         else{
-            alert("el monto solicitado es insuficiente.")
+            let card = document.createElement("div")
+            card.innerHTML=`<h3>El monto solicitado es insuficiente. El minimo es $10.000</h3>                                        `
+            document.body.append(card) 
         }
-    }    
+    } 
+    for (const prestamo of prestamos){
+        let card = document.createElement("div")
+            card.innerHTML=`<h3>Solicitaste: $${prestamo.capital}</h3>
+            <p>el interes que queres pagar es ${prestamo.interes * 100}%</p>
+            <p>en: ${prestamo.cantidadCuotas} cuotas</p>
+            <p>vas a pagar: $${prestamo.cuotaMes.toFixed(2)}</p>
+            <p>vas a devolver en total $${prestamo.devolucionTotal.toFixed(2)}</p>`
+        document.body.append(card) 
+    }   
 }
 else{
-    alert("Excediste el maximo de prestamos posibles")
+    let card = document.createElement("div")
+    card.innerHTML=`<h3>El maximo de prestamos a solicitar es 2</h3>                                        `
+    document.body.append(card)
 }
 
 
--- js/prueba.js
+++
@@ -1,3 +0,0 @@
-let titulo = document.getElementById("titulos")
-
-titulo.innerText = "no es un simulador de prestamos"
