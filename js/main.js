a27a0ea00d62ab4fdf93678a7960bf63a15e96cc
Nicolas Toranzo <toranzo.nicolas@gmail.com>
2022-07-31 14:08:02 -0300

    eventlistener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 
 
-let cantidadPrestamos = prompt("¿Cuantos prestamos queres solicitar? (Maximo 2)")
+let cantidadPrestamos = 0
+
+let formulario = document.getElementById("formulario")
+formulario.addEventListener("submit", validateForm)
+function validateForm(event){
+    event.preventDefault()
+    let form = event.target
+    cantidadPrestamos = form[0].value
+    prestamo()
+}
 
 function Prestamo(capital, cantidadCuotas, interes, cuotaMes, devolucionTotal) {
     this.capital = capital
@@ -10,63 +19,81 @@ function Prestamo(capital, cantidadCuotas, interes, cuotaMes, devolucionTotal) {
     this.devolucionTotal = devolucionTotal
 }
 
-if(cantidadPrestamos<=2){
+function prestamo(){
+    if(cantidadPrestamos<=2){
 
-    let prestamos = []
+        let prestamos = []
 
-    for(let i = 0; i < cantidadPrestamos; i++){
+        for(let i = 0; i < cantidadPrestamos; i++){
 
-        let prestamo = new Prestamo(0,0,0,0,0)
-        prestamo.capital = parseInt(prompt("ingresa el monto a pedir prestado (minimo $10.000)"))
-        prestamo.cantidadCuotas = parseInt(prompt("ingresa la cantidad de cuotas que queres pagar (maximo 60)"))
-        prestamo.interes = parseFloat(prompt("ingresa el interes anual que queres pagar")) / 100
+            let prestamo = new Prestamo(0,0,0,0,0)
 
-        function tasa (interes){
-        return (1 + interes) ** (1/12) - 1
-        }; 
-        function cuotaMensual (capital, interes, cuotas){
-        prestamo.cuotaMes = (tasa(interes) * capital) / (1 - (1 + tasa(interes)) ** - cuotas)
-        }; 
-        function devolucionTotal(cuotaMensual, cuotas){
-        prestamo.devolucionTotal = cuotaMensual * cuotas    
-        };
+            prestamo.capital = 0
+            prestamo.cantidadCuotas = 0
+            prestamo.interes = 0
 
-        if(prestamo.capital >= 10000){
-            if(prestamo.cantidadCuotas <=60){
-                tasa(prestamo.interes)
-                cuotaMensual(prestamo.capital, tasa(prestamo.interes), prestamo.cantidadCuotas)
-                devolucionTotal(prestamo.cuotaMes, prestamo.cantidadCuotas) 
-                prestamos.push(prestamo)  
+            let formPrestamo = document.getElementById("formPrestamo")
+            formPrestamo.addEventListener("submit", validateFormPrestamo)
+            function validateFormPrestamo(event){
+                event.preventDefault()
+                let formPrestamo = event.target
+                prestamo.capital = formPrestamo[0].value
+                prestamo.cantidadCuotas = formPrestamo[1].value
+                prestamo.interes = formPrestamo[2].value / 100
+                prestar()
 
-           }
-           else{
-                let card = document.createElement("div")
-                card.innerHTML=`<h3>Excediste la cantidad de cuotas habilitadas</h3>                                        `
-                document.body.append(card) 
-           } 
-        }
-        else{
-            let card = document.createElement("div")
-            card.innerHTML=`<h3>El monto solicitado es insuficiente. El minimo es $10.000</h3>                                        `
-            document.body.append(card) 
-        }
-    } 
-    for (const prestamo of prestamos){
+            }
+
+            function prestar(){
+
+                function tasa (interes){
+                return (1 + interes) ** (1/12) - 1
+                }; 
+                function cuotaMensual (capital, interes, cuotas){
+                prestamo.cuotaMes = (tasa(interes) * capital) / (1 - (1 + tasa(interes)) ** - cuotas)
+                }; 
+                function devolucionTotal(cuotaMensual, cuotas){
+                prestamo.devolucionTotal = cuotaMensual * cuotas    
+                };
+
+                if(prestamo.capital >= 10000){
+                    if(prestamo.cantidadCuotas <=60){
+                        tasa(prestamo.interes)
+                        cuotaMensual(prestamo.capital, tasa(prestamo.interes), prestamo.cantidadCuotas)
+                        devolucionTotal(prestamo.cuotaMes, prestamo.cantidadCuotas) 
+                        prestamos.push(prestamo)  
+
+                   }
+                   else{
+                        let card = document.createElement("div")
+                        card.innerHTML=`<h3>Excediste la cantidad de cuotas habilitadas</h3>                                        `
+                        document.body.append(card) 
+                   } 
+                }
+                else{
+                    let card = document.createElement("div")
+                    card.innerHTML=`<h3>El monto solicitado es insuficiente. El minimo es $10.000</h3>                                        `
+                    document.body.append(card) 
+                }
+             
+                for (const prestamo of prestamos){
+                    let card = document.createElement("div")
+                        card.innerHTML=`<h3>Solicitaste: $${prestamo.capital}</h3>
+                        <p>el interes que queres pagar es ${prestamo.interes * 100}%</p>
+                        <p>en: ${prestamo.cantidadCuotas} cuotas</p>
+                        <p>vas a pagar: $${prestamo.cuotaMes.toFixed(2)}</p>
+                        <p>vas a devolver en total $${prestamo.devolucionTotal.toFixed(2)}</p>`
+                    document.body.append(card) 
+                }   
+            }
+    }
+}
+    else{
         let card = document.createElement("div")
-            card.innerHTML=`<h3>Solicitaste: $${prestamo.capital}</h3>
-            <p>el interes que queres pagar es ${prestamo.interes * 100}%</p>
-            <p>en: ${prestamo.cantidadCuotas} cuotas</p>
-            <p>vas a pagar: $${prestamo.cuotaMes.toFixed(2)}</p>
-            <p>vas a devolver en total $${prestamo.devolucionTotal.toFixed(2)}</p>`
-        document.body.append(card) 
-    }   
+        card.innerHTML=`<h3>El maximo de prestamos a solicitar es 2</h3>                                        `
+        document.body.append(card)
+    }
 }
-else{
-    let card = document.createElement("div")
-    card.innerHTML=`<h3>El maximo de prestamos a solicitar es 2</h3>                                        `
-    document.body.append(card)
-}
-
 
 
 
