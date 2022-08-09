cebb8fed96914c9b7b2ea74c5a23f758921fb974
Nicolas Toranzo <toranzo.nicolas@gmail.com>
2022-08-09 14:27:42 -0300

    localstorage

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 let cantidadPrestamos = 0
 let name
 let nombre
+let prestamosGuardados= []
 
 const username = document.getElementById("username")
 username.addEventListener("submit", validateUsername)
@@ -16,138 +17,172 @@ function validateUsername(event) {
     
 }
 
-    function localstorage(){
-    localStorage.setItem("name",name)
-    nombre = localStorage.getItem("name")
-    }
+function localstorage(){
+localStorage.setItem("name",name)
+nombre = localStorage.getItem("name")
+}
 
-    const formulario = document.getElementById("formulario")
-    formulario.addEventListener("submit", validateForm)
-    function validateForm(event){
-        event.preventDefault()
-        let form = event.target
-        cantidadPrestamos = form[0].value
-        prestamo()
-    }
+const formulario = document.getElementById("formulario")
+formulario.addEventListener("submit", validateForm)
+function validateForm(event){
+    event.preventDefault()
+    let form = event.target
+    cantidadPrestamos = form[0].value
+    prestamo()
+}
 
-    function Prestamo(capital, cantidadCuotas, interes, cuotaMes, devolucionTotal) {
-        this.capital = capital
-        this.cantidadCuotas = cantidadCuotas
-        this.interes = interes
-        this.cuotaMes = cuotaMes
-        this.devolucionTotal = devolucionTotal
-    }
+function Prestamo(capital, cantidadCuotas, interes, cuotaMes, devolucionTotal) {
+    this.capital = capital
+    this.cantidadCuotas = cantidadCuotas
+    this.interes = interes
+    this.cuotaMes = cuotaMes
+    this.devolucionTotal = devolucionTotal
+}
 
 
-    function prestamo(){
+function prestamo(){
 
-        const contenedor = document.getElementById("contenedor")
-        const lista = document.getElementById("lista")
-        function classContenedor() {
-            contenedor.className= "container principal subtitulos p-3"
-            lista.className="list-group list-group-flush"
+    const contenedor = document.getElementById("contenedor")
+    const lista = document.getElementById("lista")
+    function classContenedor() {
+        contenedor.className= "container principal subtitulos p-3"
+        lista.className="list-group list-group-flush"
+            }
+
+    if(cantidadPrestamos<=2){
+
+        let display = document.getElementById("display")
+        display.classList.remove("d-none")
+
+        let prestamos = []
+
+        for(let i = 0; i < cantidadPrestamos -1; i++){ 
+            /*Sin el menos 1 creaba dos prestamos con el mismo input en vez
+            de esperar uno nuevo (no se porque) */
+  
+            let prestamo = new Prestamo(0,0,0,0,0)
+
+            prestamo.capital = 0
+            prestamo.cantidadCuotas = 0
+            prestamo.interes = 0
+
+            let formPrestamo = document.getElementById("formPrestamo")
+            formPrestamo.addEventListener("submit", validateFormPrestamo)
+            function validateFormPrestamo(event){
+                event.preventDefault()
+                let formPrestamo = event.target
+                prestamo.capital = formPrestamo[0].value
+                prestamo.cantidadCuotas = formPrestamo[1].value
+                prestamo.interes = formPrestamo[2].value / 100
+                prestar()
+                            
+            }
+
+        function prestar(){
+
+            function tasa (interes){
+            return (1 + interes) ** (1/12) - 1
+            }; 
+            function cuotaMensual (capital, interes, cuotas){
+            prestamo.cuotaMes = (tasa(interes) * capital) / (1 - (1 + tasa(interes)) ** - cuotas)
+            }; 
+            function devolucionTotal(cuotaMensual, cuotas){
+            prestamo.devolucionTotal = cuotaMensual * cuotas    
+            };
+                
+        if (prestamos.length < 2){
+        /*este if es para que no pueda crear mas prestamos despues de 2
+        pero si pone 1 le deja llegar a 2, falta resolver*/        
+            if(prestamo.capital >= 10000){
+                if(prestamo.cantidadCuotas <=60){
+                    tasa(prestamo.interes)
+                    cuotaMensual(prestamo.capital, tasa(prestamo.interes), prestamo.cantidadCuotas)
+                    devolucionTotal(prestamo.cuotaMes, prestamo.cantidadCuotas) 
+                    prestamos.push(prestamo)
+                    console.log(prestamos)//esto despues hay que borrarlo
+
                 }
-
-        if(cantidadPrestamos<=2){
-
-            let display = document.getElementById("display")
-            display.classList.remove("d-none")
-
-            let prestamos = []
-
-            for(let i = 0; i < cantidadPrestamos -1; i++){ 
-                /*Sin el menos 1 creaba dos prestamos con el mismo input en vez
-                de esperar uno nuevo (no se porque) */
-      
-                let prestamo = new Prestamo(0,0,0,0,0)
-
-                prestamo.capital = 0
-                prestamo.cantidadCuotas = 0
-                prestamo.interes = 0
-
-                let formPrestamo = document.getElementById("formPrestamo")
-                formPrestamo.addEventListener("submit", validateFormPrestamo)
-                function validateFormPrestamo(event){
-                    event.preventDefault()
-                    let formPrestamo = event.target
-                    prestamo.capital = formPrestamo[0].value
-                    prestamo.cantidadCuotas = formPrestamo[1].value
-                    prestamo.interes = formPrestamo[2].value / 100
-                    prestar()
-                                
-                }
-
-            function prestar(){
-
-                function tasa (interes){
-                return (1 + interes) ** (1/12) - 1
-                }; 
-                function cuotaMensual (capital, interes, cuotas){
-                prestamo.cuotaMes = (tasa(interes) * capital) / (1 - (1 + tasa(interes)) ** - cuotas)
-                }; 
-                function devolucionTotal(cuotaMensual, cuotas){
-                prestamo.devolucionTotal = cuotaMensual * cuotas    
-                };
-                    
-            if (prestamos.length < 2){
-            /*este if es para que no pueda crear mas prestamos despues de 2
-            pero si pone 1 le deja llegar a 2, falta resolver*/        
-                if(prestamo.capital >= 10000){
-                    if(prestamo.cantidadCuotas <=60){
-                        tasa(prestamo.interes)
-                        cuotaMensual(prestamo.capital, tasa(prestamo.interes), prestamo.cantidadCuotas)
-                        devolucionTotal(prestamo.cuotaMes, prestamo.cantidadCuotas) 
-                        prestamos.push(prestamo)
-                        console.log(prestamos)//esto despues hay que borrarlo
-
-                    }
-                    else{                        
-                        contenedor.innerHTML= nombre + ", excediste la cantidad de cuotas habilitadas"
-                        classContenedor()
-                    } 
-                }
-                else{
-                    contenedor.innerHTML= nombre + ", el monto solicitado es insuficiente. El minimo es $10.000"
+                else{                        
+                    contenedor.innerHTML= nombre + ", excediste la cantidad de cuotas habilitadas"
                     classContenedor()
-                }
+                } 
             }
             else{
-              let maxPrestamo = document.getElementById("maxPrestamo")
-              maxPrestamo.className="container principal subtitulos p-3 mb-3"
-              maxPrestamo.innerText= nombre + ", alcanzaste el maximo de prestamos posibles"
-            }    
-                 
-                for (const prestamo of prestamos){
-
-                    classContenedor()
-                    let node= document.createElement("li")
-                    node.className= "list-group-item"                        
-                    node.innerHTML = `<h3>Solicitaste: 
-                    $${prestamo.capital}</h3>
-                    <p>El interes que queres pagar es ${(prestamo.interes * 100).toFixed(2)}%</p>
-                    <p>En: ${prestamo.cantidadCuotas} cuotas</p>
-                    <p>Cada cuota será de: 
-                    $${prestamo.cuotaMes.toLocaleString(undefined, {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2})}</p>
-                    <p>Vas a devolver en total 
-                        $${prestamo.devolucionTotal.toLocaleString(undefined, {
-                            minimumFractionDigits: 2,
-                            maximumFractionDigits: 2})}</p>`
-                    lista.append(node)
-                    contenedor.append(lista)
-                                        
-                }   
+                contenedor.innerHTML= nombre + ", el monto solicitado es insuficiente. El minimo es $10.000"
+                classContenedor()
             }
         }
-    }
         else{
-            contenedor.innerHTML= nombre + ', El maximo de prestamos a solicitar es 2'
-            classContenedor()
+          let maxPrestamo = document.getElementById("maxPrestamo")
+          maxPrestamo.className="container principal subtitulos p-3 mb-3"
+          maxPrestamo.innerText= nombre + ", alcanzaste el maximo de prestamos posibles"
+        }    
+             
+            for (const prestamo of prestamos){
+
+                classContenedor()
+                let node= document.createElement("li")
+                node.className= "list-group-item"                        
+                node.innerHTML = `<h3>Solicitaste: 
+                $${prestamo.capital}</h3>
+                <p>El interes que queres pagar es ${(prestamo.interes * 100).toFixed(2)}%</p>
+                <p>En: ${prestamo.cantidadCuotas} cuotas</p>
+                <p>Cada cuota será de: 
+                $${prestamo.cuotaMes.toLocaleString(undefined, {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2})}</p>
+                <p>Vas a devolver en total 
+                    $${prestamo.devolucionTotal.toLocaleString(undefined, {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2})}</p>`
+                lista.append(node)
+                contenedor.append(lista)
+                saveButton.className="container pretty-button"
+                                    
+            }   
         }
+
+        let saveButton = document.getElementById("save-button")
+        saveButton.addEventListener("click", guardarPrestamos)
+
+        function guardarPrestamos(){
+
+            
+        prestamosGuardados.push(prestamos)
+        console.log(prestamosGuardados)
+        localStorage.setItem("guardaPrestamos", JSON.stringify(prestamosGuardados))
+        }
+
     }
+}
+    else{
+        contenedor.innerHTML= nombre + ', El maximo de prestamos a solicitar es 2'
+        classContenedor()
+    }
+}
+
+function cargarPrestamos(){
+    let prestamosAnteriores = JSON.parse(localStorage.getItem("guardaPrestamos"))
+    console.log(prestamosAnteriores)/*
+    
+                let node= document.createElement("li")
+                node.className= "list-group-item"                        
+                node.innerHTML = `<h3>Solicitaste: 
+                $${prestamosGuardados.capital}</h3>
+                <p>El interes que queres pagar es ${(prestamosGuardados.interes * 100).toFixed(2)}%</p>
+                <p>En: ${prestamosGuardados.cantidadCuotas} cuotas</p>
+                <p>Cada cuota será de: 
+                $${prestamosGuardados.cuotaMes}</p>
+                <p>Vas a devolver en total 
+                    $${prestamosGuardados.devolucionTotal}</p>`
+                lista.append(node)
+                contenedor.append(lista)*/
 
 
+}
+
+cargarPrestamos()
+ 
 
 
 
